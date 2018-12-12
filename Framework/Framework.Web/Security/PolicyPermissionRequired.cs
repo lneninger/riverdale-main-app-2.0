a1d04adf8274443fe1fa4071e06565df1cf0a6dd
lneninger <lneninger@hipalanet.com>
2018-12-12 10:33:50 -0500

    Improve Authorization mechanism. Fix Policies generation

---
+++ Framework/Framework.Web/Security/PolicyPermissionRequired.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Web.Security
+{
+    public class PolicyPermissionRequired : IAuthorizationRequirement
+    {
+        public string Permission { get; private set; }
+
+        public PolicyPermissionRequired(string permission)
+        {
+            this.Permission = permission;
+        }
+
+        public static void BuildPolicies(AuthorizationOptions options, IEnumerable<string> permissionNames)
+        {
+            foreach (var permissionName in permissionNames)
+            {
+                options.AddPolicy(permissionName, policy => policy.Requirements.Add(new PolicyPermissionRequired(permissionName)));
+            }
+        }
+    }
+}
---
+++ Framework/Framework.Web/Security/PolicyPermissionRequiredHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Web.Security
+{
+    public class PolicyPermissionRequiredHandler : AuthorizationHandler<PolicyPermissionRequired>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PolicyPermissionRequired requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == JwtConstants.Strings.JwtClaimIdentifiers.Permissions))
+            {
+                //TODO: Use the following if targeting a version of
+                //.NET Framework older than 4.6:
+                //      return Task.FromResult(0);
+                return Task.CompletedTask;
+            }
+
+            var permissions = context.User.FindAll(JwtConstants.Strings.JwtClaimIdentifiers.Permissions);//  FindFirst(c => c.Type == ClaimTypes.DateOfBirth && c.Issuer == "http://contoso.com").Value);
+
+            var matchClaimCount = permissions.Count(permission => permission.Value == requirement.Permission);
+
+            if (matchClaimCount > 0)
+            {
+                context.Succeed(requirement);
+            }
+
+            //TODO: Use the following if targeting a version of
+            //.NET Framework older than 4.6:
+            //      return Task.FromResult(0);
+            return Task.CompletedTask;
+        }
+    }
+}
