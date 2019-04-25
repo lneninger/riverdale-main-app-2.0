be9abc8b83b054827fddd3748824248a9132ee4a
lneninger <lneninger@hipalanet.com>
2019-04-25 07:12:16 -0400

    Add firebase project config json

--- WebLandingPage/src/environments/environment.prod.ts
+++ WebLandingPage/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBoS6ADY2ivN5rmpkHyX5B2xo4TKiReBHs",
+    authDomain: "hipalanet-test.firebaseapp.com",
+    databaseURL: "https://hipalanet-test.firebaseio.com",
+    projectId: "hipalanet-test",
+    storageBucket: "hipalanet-test.appspot.com",
+    messagingSenderId: "309802452085"
+  }
 };
