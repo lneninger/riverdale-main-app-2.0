130266caa59c978f63cc0e669b0b451457bad0ad
lneninger <lneninger@hipalanet.com>
2019-04-29 00:28:05 -0400

    newsletters trigger firebase function

--- WebLandingPage/functions/src/index.ts
+++ WebLandingPage/functions/src/index.ts
@@ -1,8 +1,33 @@
 import * as functions from 'firebase-functions';
 
+
+const nodemailer = require('nodemailer');
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const watchNewNewslettersSubscriptions = functions.database.ref('/newsletters_subscriptions').onCreate((snapshot, context) => {
+
+  console.log(`Executing watchNewNewslettersSubscriptions`);
+  console.log(functions.config());
+  const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+  const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+  const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
+
+  const subscription = snapshot.val();
+
+  const mailOptions = {
+    to: subscription.email,
+    subject: `Subscription received`,
+    html: `We received your subscription of our newsletters`
+  };
+
+  return mailTransport.sendMail(mailOptions).then(() => console.log(`Mail sent to: ${subscription.email}`));
+
+});
+
--- WebLandingPage/src/assets/js/script.js
+++ WebLandingPage/src/assets/js/script.js
@@ -50,65 +50,65 @@ $(document).ready(function() {
    color picker start
    ----------------------------------------*/
 
-  (function() {
-    $('<div class="color-picker">' +
-      '<a href="#" class="handle">' +
-      '<i class="fa fa-cog"></i>' +
-      '</a><div class="sec-position"><div class="settings-header">' +
-      '<h3>Select Color:</h3>' +
-      '</div>' +
-      '<div class="section">' +
-      '<div class="colors o-auto">' +
-      '<a href="#" class="color-1" ></a>' +
-      '<a href="#" class="color-2" ></a>' +
-      '<a href="#" class="color-3" ></a>' +
-      '</div>' +
-      '</div>' +
-      '</div>' +
-      '</div>').appendTo($('body'));
-  })();
-  var body_event = $("body");
-  body_event.on("click", ".color-1", function() {
-    var link = $("<link />", {
-      rel: "stylesheet",
-      type: "text/css",
-      href: "assets/css/color/color-1.css"
-    });
+  //(function() {
+  //  $('<div class="color-picker">' +
+  //    '<a href="#" class="handle">' +
+  //    '<i class="fa fa-cog"></i>' +
+  //    '</a><div class="sec-position"><div class="settings-header">' +
+  //    '<h3>Select Color:</h3>' +
+  //    '</div>' +
+  //    '<div class="section">' +
+  //    '<div class="colors o-auto">' +
+  //    '<a href="#" class="color-1" ></a>' +
+  //    '<a href="#" class="color-2" ></a>' +
+  //    '<a href="#" class="color-3" ></a>' +
+  //    '</div>' +
+  //    '</div>' +
+  //    '</div>' +
+  //    '</div>').appendTo($('body'));
+  //})();
+  //var body_event = $("body");
+  //body_event.on("click", ".color-1", function() {
+  //  var link = $("<link />", {
+  //    rel: "stylesheet",
+  //    type: "text/css",
+  //    href: "assets/css/color/color-1.css"
+  //  });
 
-    $('#color').html(link);
-    $('#color-admin').html(link);
-    return false;
-  });
-  body_event.on("click", ".color-2", function() {
-    var link = $("<link />", {
-      rel: "stylesheet",
-      type: "text/css",
-      href: "assets/css/color/color-2.css"
-    });
-    $('#color').html(link);
-    $('#color-admin').html(link);
-    return false;
-  });
-  body_event.on("click", ".color-3", function() {
-    var link = $("<link />", {
-      rel: "stylesheet",
-      type: "text/css",
-      href: "assets/css/color/color-3.css"
-    });
-    $('#color').html(link);
-    $('#color-admin').html(link);
-    return false;
-  });
-  $('.color-picker').animate({ right: '-190px' });
-  body_event.on("click", ".color-picker a.handle", function(e) {
-    e.preventDefault();
-    var div = $('.color-picker');
-    if (div.css('right') === '-190px') {
-      $('.color-picker').animate({ right: '0px' });
-    } else {
-      $('.color-picker').animate({ right: '-190px' });
-    }
-  });
+  //  $('#color').html(link);
+  //  $('#color-admin').html(link);
+  //  return false;
+  //});
+  //body_event.on("click", ".color-2", function() {
+  //  var link = $("<link />", {
+  //    rel: "stylesheet",
+  //    type: "text/css",
+  //    href: "assets/css/color/color-2.css"
+  //  });
+  //  $('#color').html(link);
+  //  $('#color-admin').html(link);
+  //  return false;
+  //});
+  //body_event.on("click", ".color-3", function() {
+  //  var link = $("<link />", {
+  //    rel: "stylesheet",
+  //    type: "text/css",
+  //    href: "assets/css/color/color-3.css"
+  //  });
+  //  $('#color').html(link);
+  //  $('#color-admin').html(link);
+  //  return false;
+  //});
+  //$('.color-picker').animate({ right: '-190px' });
+  //body_event.on("click", ".color-picker a.handle", function(e) {
+  //  e.preventDefault();
+  //  var div = $('.color-picker');
+  //  if (div.css('right') === '-190px') {
+  //    $('.color-picker').animate({ right: '0px' });
+  //  } else {
+  //    $('.color-picker').animate({ right: '-190px' });
+  //  }
+  //});
 
 
 });
